5d0a10ecf8253dbc9bad39ca410ddca89659068e
Ayushmaan0101 <ayushmaansingh304@gmail.com>
2024-05-12 16:46:02 +0530

    updated

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -26,14 +26,14 @@ const Theme = () => {
   }, []); 
  
   return (
-    <div id='ThemeSection' className={`py-10 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via-indigo-800 to-violet-600`}>
+    <div id='ThemeSection' className={`py-16 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via-indigo-800 to-violet-600`}>
       <motion.h1 ref={ref} style={{ scale: scaleProgress, opacity: scrollYProgress }} className='text-[23px] md:text-4xl lg:px-80 sm:px-40 text-center font-bold text-white mb-8 ' transition={{ duration: 0.5 }}>
         Make Coding Cooler Than the
         Other Side of the Pillow.
       </motion.h1>
       <div className='flex justify-center'>
         <div className='w-full max-w-4xl  '>
-          <Image src={showIce ? Ice : Blaze} alt={showIce ? 'Ice Theme Preview' : 'Blaze Theme Preview'} className='p-3 rounded-2xl mb-10' />
+          <Image src={showIce ? Ice : Blaze} alt={showIce ? 'Ice Theme Preview' : 'Blaze Theme Preview'} className='p-3 rounded-2xl mb-5' />
         </div>
       </div>
     </div>
