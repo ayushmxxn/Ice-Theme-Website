676169a62553215264d27df2daf684a0226232fa
Ayushmaan0101 <ayushmaansingh304@gmail.com>
2024-05-12 16:48:55 +0530

    updated

--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -26,7 +26,7 @@ const Theme = () => {
   }, []); 
  
   return (
-    <div id='ThemeSection' className={`py-16 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via-indigo-800 to-violet-600`}>
+    <div id='ThemeSection' className={`py-12 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-gray-900 via-indigo-800 to-violet-600`}>
       <motion.h1 ref={ref} style={{ scale: scaleProgress, opacity: scrollYProgress }} className='text-[23px] md:text-4xl lg:px-80 sm:px-40 text-center font-bold text-white mb-8 ' transition={{ duration: 0.5 }}>
         Make Coding Cooler Than the
         Other Side of the Pillow.
